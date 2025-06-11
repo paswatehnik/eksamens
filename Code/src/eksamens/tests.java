0b3cc03b433f9d409f88f6088f95f5f0d25bd18c
paswatehnik <mihailpetrovich789@gmail.com>
2025-06-11 14:01:05 +0300

    izveidoju macibu materiala ar informaciju par aretmetiskiem operatoriem matematiskiem funkcijam un random skailu generesanu, tris uzdevumu piemeri, atteli, tabulas un izmantotie avoti

--- Code/src/eksamens/tests.java
+++ Code/src/eksamens/tests.java
@@ -1,10 +1,21 @@
 package eksamens;
+import java.util.Random;
 
 public class tests {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Random rand = new Random();
+        
+        int garums = rand.nextInt(16) + 5;
+        int platums = rand.nextInt(13) + 3;
+        
+        int laukums = garums * platums;
+        int perimetrs = 2 * (garums + platums);
+        
+        System.out.println("Taisnstūra parametri:");
+        System.out.println("Garums: " + garums);
+        System.out.println("Platums: " + platums);
+        System.out.println("------------------");
+        System.out.println("Laukums: " + laukums);
+        System.out.println("Perimetrs: " + perimetrs);
+    }
 }
